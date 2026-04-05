bb77b02fe362d446a723805748bd61dd83335f9a
useriswave <121168867+useriswave@users.noreply.github.com>
2026-04-05 11:37:33 +0300

    solved question 3

---
+++ LabAssessmentRevision1/src/main/java/lab1/Q3.java
@@ -0,0 +1,28 @@
+package lab1;
+
+import java.util.ArrayList;
+
+public class Q3 {
+    public static void main(String[] args) {
+        var list = new ArrayList<Integer>();
+
+        int[] nta = {5, 1, 2, 2, 3, 5, 4, 1};
+
+        for (int n : nta) {
+            list.add(n);
+        }
+
+        System.out.println(removeDuplicates(list));
+    }
+
+    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> arr) {
+        var newList = new ArrayList<Integer>();
+        for (int num : arr) {
+            if (!newList.contains(num)) {
+                newList.add(num);
+            }
+        }
+        return newList;
+    }
+}
+
