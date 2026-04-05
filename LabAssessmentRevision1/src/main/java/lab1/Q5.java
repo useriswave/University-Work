b7302049926849d6955d8b207e99a3f5e697d437
useriswave <121168867+useriswave@users.noreply.github.com>
2026-04-05 11:38:01 +0300

    solved question 5

---
+++ LabAssessmentRevision1/src/main/java/lab1/Q5.java
@@ -0,0 +1,24 @@
+package lab1;
+
+import java.util.ArrayList;
+
+public class Q5 {
+    public static void main(String[] args) {
+        var list = new ArrayList<String>();
+        String[] wToAdd = {"A", "B", "C", "D"};
+
+        for(String w : wToAdd) {
+            list.add(w);
+        }
+
+        System.out.println(reverseList(list));
+    }
+
+    public static ArrayList<String> reverseList(ArrayList<String> arr) {
+        var nList = new ArrayList<String>(0);
+        for(int i = arr.size()-1; i >= 0; i--) {
+            nList.add(arr.get(i));
+        }
+        return nList;
+    }
+}
