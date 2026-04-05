ae070ade891c4ed906e37c2c9fcdc6a076d171f6
useriswave <121168867+useriswave@users.noreply.github.com>
2026-04-05 11:45:40 +0300

    restored for loop patterns class

--- LabAssessmentRevision1/src/main/java/lab1/Q1.java
+++ LabAssessmentRevision1/src/main/java/lab1/Q1.java
@@ -2,7 +2,7 @@ package lab1;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
